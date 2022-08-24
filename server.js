30ae73986f40000a6c494f17d1c09e2f09c8cacf
sabin149 <dangalsabin149@gmail.com>
2022-08-25 00:59:43 +0545

    fixed some errors

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const compression = require('compression');
 const app = express();
 
 app.use(express.json());
-app.use(cors(
-  {
-    origin: "https://frontend-emedicine-platform.herokuapp.com",
-    credentials: true,
-  }));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"))
