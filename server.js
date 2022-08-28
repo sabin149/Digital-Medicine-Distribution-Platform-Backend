30dc27b5abbfbf75c6b086c6802ecd9a5378617e
sabin149 <dangalsabin149@gmail.com>
2022-08-28 17:47:29 +0545

    fixed some errors

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const compression = require('compression');
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"))
