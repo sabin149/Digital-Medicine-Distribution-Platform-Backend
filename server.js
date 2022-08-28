366d87393a54adc3fcab50226807693f7d13c853
sabin149 <dangalsabin149@gmail.com>
2022-08-28 18:18:38 +0545

    fixed some errors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://frontend-emedicine-platform.herokuapp.com/",
+  origin: "https://frontend-emedicine-platform.herokuapp.com",
   credentials: true,
 }));
 
